511d25da06110d000f7022c45bdcfca8c355ecd6
Edufemartins <edufemartins@yahoo.com.br>
2024-08-22 16:36:09 -0300

    terceira alteração

--- src/index.js
+++ src/index.js
@@ -11,18 +11,42 @@
 //Ao final deve se exibir uma mensagem: "O Herói de nome **{nome}** está no nível de **{nivel}**"
 
 
-
 let nomeHeroi = "Eduardo"
-let experiencia = 999
-
-if(experiencia < 1000){
-    console.log("O Herói de nome " + nomeHeroi + " esta no nível Ferro");
-}
-else if(experiencia > 1001 && 2000)
-    {
-    console.log("O Herói de nome " + nomeHeroi + " esta no nível Bronze");
-}
-else( experiencia > 2001 && 5000)
+let experiencia = 4554
+​
+if(experiencia <= 1000)
 {
-    console.log("O Herói de nome " + nomeHeroi + " esta no nível Prata");
+    console.log("O Herói de nome " + nomeHeroi + " esta no nível Ferro")
 }
+else if(experiencia > 1001 && experiencia <= 2000)
+{
+    console.log("O Herói de nome " + nomeHeroi + " esta no nível Bronze")
+}
+else if(experiencia >= 2001 && experiencia <= 5000)
+{
+    console.log("O Herói de nome " + nomeHeroi + " esta no nível Prata")
+}
+else if(experiencia > 5001 && experiencia <= 7000)
+{
+    console.log("O Herói de nome " + nomeHeroi + " esta no nível Ouro")
+}
+else if(experiencia > 7001 && experiencia <= 8000)
+{
+    console.log("O Herói de nome " + nomeHeroi + " esta no nível Platina")
+}
+else if(experiencia > 8001 && experiencia <= 9000)
+{
+    console.log("O Herói de nome " + nomeHeroi + " esta no nível Ascendente")
+}
+else if(experiencia > 9001 && experiencia <= 10000)
+{
+    console.log("O Herói de nome " + nomeHeroi + " esta no nível Imortal")
+}
+else if(experiencia > 10001)
+{
+    console.log("O Herói de nome " + nomeHeroi + " esta no nível Radiante")
+}
+else{
+​
+}
+​
