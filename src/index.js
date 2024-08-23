8728981499e2fc442ac506f75764f658168d5491
Edufemartins <edufemartins@yahoo.com.br>
2024-08-23 06:57:12 -0300

    quarta alteração

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ if(experiencia <= 1000)
 {
     console.log("O Herói de nome " + nomeHeroi + " esta no nível Ferro")
 }
-else if(experiencia > 1001 && experiencia <= 2000)
+else if(experiencia >= 1001 && experiencia <= 2000)
 {
     console.log("O Herói de nome " + nomeHeroi + " esta no nível Bronze")
 }
@@ -26,27 +26,27 @@ else if(experiencia >= 2001 && experiencia <= 5000)
 {
     console.log("O Herói de nome " + nomeHeroi + " esta no nível Prata")
 }
-else if(experiencia > 5001 && experiencia <= 7000)
+else if(experiencia >= 5001 && experiencia <= 7000)
 {
     console.log("O Herói de nome " + nomeHeroi + " esta no nível Ouro")
 }
-else if(experiencia > 7001 && experiencia <= 8000)
+else if(experiencia >= 7001 && experiencia <= 8000)
 {
     console.log("O Herói de nome " + nomeHeroi + " esta no nível Platina")
 }
-else if(experiencia > 8001 && experiencia <= 9000)
+else if(experiencia >= 8001 && experiencia <= 9000)
 {
     console.log("O Herói de nome " + nomeHeroi + " esta no nível Ascendente")
 }
-else if(experiencia > 9001 && experiencia <= 10000)
+else if(experiencia >= 9001 && experiencia <= 10000)
 {
     console.log("O Herói de nome " + nomeHeroi + " esta no nível Imortal")
 }
-else if(experiencia > 10001)
+else if(experiencia >= 10001)
 {
     console.log("O Herói de nome " + nomeHeroi + " esta no nível Radiante")
 }
 else{
-​
+​   console.log()
 }
 ​
